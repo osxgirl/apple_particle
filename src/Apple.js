d28e69e5b11586759fa9202d97fe4e16c8ac4c80
osxgirl <osxgirl@mac.com>
2021-08-07 22:58:14 -0400

    //💜

--- src/Apple.js
+++ src/Apple.js
@@ -6,16 +6,16 @@ const particleOptions: ParticleOptions = {
     // Get pixel
     const pixel = image.get(x, y);
     // Make a particle for this pixel if blue > 50 (range 0-255)
-    return pixel.b > 50;
+    return pixel.b > 10;
   },
-  color: ({ x, y, image }) => "#61dafb"
+  color: ({ x, y, image }) => "ff11f0"
 };
 
 export default function App() {
   return (
     <ParticleImage
       src={"/react-logo.png"}
-      scale={0.95}
+      scale={0.55}
       entropy={40}
       maxParticles={4200}
       particleOptions={particleOptions}
